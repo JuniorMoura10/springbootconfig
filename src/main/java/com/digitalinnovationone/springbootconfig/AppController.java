1f5b68cf7eff73b29f3c03e4ec3d5c8e7fd7d3d8
Junior Moura <geraldomartins10@gmail.com>
2022-01-14 12:09:24 -0300

    Mais alguns testes feitos, como definir uma variável de ambiente.

--- src/main/java/com/digitalinnovationone/springbootconfig/AppController.java
+++ src/main/java/com/digitalinnovationone/springbootconfig/AppController.java
@@ -10,8 +10,16 @@ public class AppController {
     @Value("${app.message}")
     private String appMessage;
 
+    @Value("${ENV_DB_URL:NENHUMA}")
+    private String dbEnviromentVariable;
+
     @GetMapping("/")
     public String getAppMessage(){
         return appMessage;
+    }
+
+    @GetMapping("/envVariable")
+    public String getEnviromentVariable(){
+        return "A seguinte variável de ambiente foi passada: " + dbEnviromentVariable;
     }
 }
